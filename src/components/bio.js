3b2b1fa1b8baaa54543d44a3d11c5d20bd3b85ee
tamakiii <tamakiii@users.noreply.github.com>
2020-05-20 08:36:21 +0900

    fix author etc

--- src/components/bio.js
+++ src/components/bio.js
@@ -14,7 +14,7 @@ import { rhythm } from "../utils/typography"
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      avatar: file(absolutePath: { regex: "/profile.png/" }) {
         childImageSharp {
           fixed(width: 50, height: 50) {
             ...GatsbyImageSharpFixed
@@ -56,13 +56,8 @@ const Bio = () => {
           borderRadius: `50%`,
         }}
       />
-      <p>
-        Written by <strong>{author.name}</strong> {author.summary}
-        {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
-      </p>
+      <p>{author.summary}</p>
+      <p><strong><a href={`https://twitter.com/${social.twitter}`}>@{author.name}</a></strong></p>
     </div>
   )
 }
